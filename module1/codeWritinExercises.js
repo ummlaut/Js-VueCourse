d02944a5079e62d9e01d7660922c67f3d13e5f30
ummlaut <iachmiz@gmail.com>
2021-04-01 10:51:52 +0300

    functions writing exercises 1-4 with remarks

--- module1/codeWritinExercises.js
+++ module1/codeWritinExercises.js
@@ -1,6 +1,7 @@
 // 1
 
 function multiply(x) {
+  if (!arguments.length) return 0;
   res = 1;
   for (let i = 0; i < arguments.length; i++) {
     res *= arguments[i];
@@ -10,11 +11,14 @@ function multiply(x) {
 
 // 2
 function reverseString(str) {
+  let string = String(str);
   let res = "";
   for (let i = str.length; i--; ) {
     res += str[i];
   }
   console.log(res);
+
+  return res;
 }
 
 // 3
@@ -22,26 +26,37 @@ function unicodeString(arg) {
   let str = String(arg);
   let res = "";
   for (let i = 0; i < str.length; i++) {
-    res += str.charCodeAt(i) + " ";
+    res += str[i].charCodeAt() + " ";
   }
-  res.trim();
-  console.log(res);
-  return res;
+
+  return res.trim();
 }
 
 //4
 function guessTheNumber(num) {
-  if (typeof num === "string") {
-    parseInt(num);
-  }
-  if (typeof num !== "number") {
+  const number = Number(num);
+  if (typeof number !== "number" || isNaN(number)) {
     return new Error("Please provide a valid number");
   }
-  if (num < 0 && num > 10) {
+  if (number < 0 && number > 10) {
     return new Error("Please provide number in range 0 - 10");
   }
-  let rnd = Math.floor(Math.random() * 10 + 1);
+  const rnd = Math.floor(Math.random() * 10 + 1);
   return rnd === num
     ? "You win"
     : `You are lose, your number is ${num}, the random number is ${rnd} `;
+}
+function guessTheNumber(num) {
+  const number = Number(num);
+
+  if (typeof number !== "number" || isNaN(number))
+    return new Error("Please provide a valid number");
+  if (number < 0 || number > 10)
+    return new Error("Please provide number in range 0 - 10");
+
+  const random = Math.ceil(Math.random() * 10);
+
+  if (random === number) return "You win!";
+
+  return `You are lose, your number is ${number}, the random number is ${random}`;
 }
