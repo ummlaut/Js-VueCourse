1dba31067edd1e42584918a745930aac52b8135b
ummlaut <iachmiz@gmail.com>
2021-04-02 17:01:53 +0300

    closures 3,4

--- module1/closures.js
+++ module1/closures.js
@@ -11,9 +11,9 @@ const minusNum = minusWrap(10);
 minusNum(6);
 
 //
-function multiplyMaker (n){
-  const multiplyRes = function(x){
-    return n*=x;
+function multiplyMaker(n) {
+  const multiplyRes = function (x) {
+    return (n *= x);
   };
   return multiplyRes;
 }
@@ -29,3 +29,67 @@ multiply(3); // 12 (4 * 3)
 multiply(10); // 120 (12 * 10)
 
 //3
+function strTricks() {
+  let newStr = "";
+  return {
+    installString(str = "") {
+      newStr = String(str);
+      return str;
+    },
+    getString() {
+      return newStr;
+    },
+    getStringLength() {
+      return newStr.length;
+    },
+    getReversedString() {
+      return newStr.split("").reverse().join();
+    },
+  };
+}
+
+const newString = strTricks();
+newString.getString("salkffm2o");
+
+//4
+const calculator = (function () {
+  let num = 0;
+
+  function setNum(val = 0) {
+    num = Number(val);
+    return this;
+  }
+  function plus(val) {
+    num += Number(val);
+    return this;
+  }
+  function minus(val) {
+    num -= Number(val);
+    return this;
+  }
+  function multiply(val) {
+    num *= Number(val);
+    return this;
+  }
+  function division(val) {
+    num /= Number(val);
+    return this;
+  }
+  function exponention(exp = 0) {
+    num = Math.pow(num, exp);
+    return this;
+  }
+  function getNum() {
+    return Number(num.toFixed(2));
+  }
+  return {
+    setNum,
+    plus,
+    minus,
+    multiply,
+    division,
+    exponention,
+    getNum,
+  };
+})();
+console.log(calculator.setNum(3).exponention(3).getNum());
