f60932a26b51721b0704dfff76ec792f5c37fe83
ummlaut <iachmiz@gmail.com>
2021-04-05 10:54:54 +0300

    destructure tasks 3,4

--- module1/destructure.js
+++ module1/destructure.js
@@ -60,3 +60,43 @@ function getInfo(
 getInfo(organisation)
 getInfo()
 */
+
+//3
+let user = {
+  guid: "dd969d30-841d-436e-9550-3b0c649e4d34",
+  isActive: false,
+  balance: "$2,474.46",
+  age: 30,
+  eyeColor: "blue",
+  name: "Tameka Maxwell",
+  gender: "female",
+  company: "ENOMEN",
+  email: "tamekamaxwell@enomen.com",
+  phone: "+1 (902) 557-3898",
+  tags: ["aliquip", "anim", "exercitation", "non"],
+  friends: [
+    {
+      id: 0,
+      name: "Barber Hicks",
+    },
+    {
+      id: 1,
+      name: "Santana Cruz",
+    },
+    {
+      id: 2,
+      name: "Leola Cabrera",
+    },
+  ],
+};
+
+const {
+  name = "",
+  email = "",
+  balance = "",
+  tags: [firstTag = "", , , lastTag = ""] = [],
+  friends: [{ name: friendName = "" }] = [],
+} = user;
+
+//4
+const newArr = [...user.tags, ...user.friends];
