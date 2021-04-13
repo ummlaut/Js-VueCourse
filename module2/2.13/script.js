13117b20ba19fb1ce1b66a0f6083a95f205bfe74
ummlaut <iachmiz@gmail.com>
2021-04-14 01:01:50 +0300

    module 2 task 13 remarks

--- module2/2.13/script.js
+++ module2/2.13/script.js
@@ -5,3 +5,37 @@ const DOMInformation = (node) => console.dir(node);
 //3
 const filteredLinks = [...document.links].filter((a) => a.closest("ul"));
 filterText = filteredLinks.map(filteredLinks.textContent);
+
+//prepod solutions
+//2
+function getNodeInfo(node) {
+  if (!(node instanceof Element)) return;
+
+  return {
+    nodeType: node.nodeType,
+    tagName: node.tagName,
+    childNodes: node.childNodes.length,
+  };
+}
+
+const info = getNodeInfo(p);
+console.log(info);
+
+//3
+const list = document.querySelector("ul");
+
+function getTextFromUl(ul) {
+  if (!(ul instanceof HTMLElement)) return;
+  const [...links] = ul.querySelectorAll("a") || [];
+  return links.map((a) => a.textContent);
+}
+
+console.log(getTextFromUl(list));
+
+//4
+const [...pChildNodes] = document.querySelector("p").childNodes;
+pChildNodes.forEach((child) => {
+  if (child.nodeType === 3) {
+    child.textContent = "-text-";
+  }
+});
