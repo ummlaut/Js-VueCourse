f6ba7ac0578ddc310507ac6c7125f211e31106d5
ummlaut <iachmiz@gmail.com>
2021-04-13 22:57:15 +0300

    module 2 task 1

--- module2/2.1/dom.js
+++ module2/2.1/dom.js
@@ -12,3 +12,21 @@ console.log(head);
 console.log(body);
 console.log(bodyChildren);
 console.log(firstDiv, firstDivChildren);
+
+//prepod solution
+//1
+console.log(document.head);
+//2
+console.log(document.body);
+//3
+console.log(document.body.children);
+//4.а
+console.log(document.body.firstElementChild);
+console.log(document.body.firstElementChild.children);
+// 4.б
+const div = document.body.firstElementChild;
+// При помощи оператора ... мы получаем из коллекции массив который фильтруем сравнивая каждый элемент с первым и последним элементом в div.
+const filteredArticles = [...div.children].filter(
+  (p) => p !== div.firstElementChild && p !== div.lastElementChild
+);
+console.log(filteredArticles);
--- module2/2.2/dom.js
+++ module2/2.2/dom.js
@@ -1,9 +1,10 @@
+//2.2
 const isParent = function (parent, child) {
   if (parent instanceof Node && child instanceof Node) {
     return parent === child.parentNode ? true : false;
   }
 };
-
+//2.3
 const links = document.querySelectorAll("a");
 const linksArray = Array.from(links);
 for (let i = 0; i < linksArray.length; i++) {
@@ -12,6 +13,39 @@ for (let i = 0; i < linksArray.length; i++) {
   }
 }
 console.log(linksArray);
+
+//2.4
 const ul = document.querySelector("ul");
 ulPrev = ul.previousElementSibling;
 ulNext = ul.nextElementSibling;
+
+//prepod solutions
+ function isParent(parent, child) {
+   // Проверяем что переданные элементы являются HTML элементами
+   if (!(parent instanceof HTMLElement) || !(child instanceof HTMLElement))
+     return;
+
+   let isParent = false;
+   let currentParent = child.parentElement;
+
+   while (currentParent) {
+     isParent = currentParent === parent;
+     if (isParent) {
+       break;
+     }
+
+     currentParent = currentParent.parentElement;
+   }
+
+   return isParent;
+ }
+
+ isParent(document.body.children[0], document.querySelector("mark"));
+
+ //3
+const filteredLinks = [...document.links].filter((a) => !a.closest("ul"));
+console.log(filteredLinks);
+
+//const ul = document.querySelector('ul');
+const prev = ul.previousElementSibling;
+const next = ul.nextElementSibling;
