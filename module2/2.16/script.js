63947f3e664a093636397aed2dbe0170aff57a9d
ummlaut <iachmiz@gmail.com>
2021-04-14 22:56:17 +0300

    module 2 hw 16 full with remarks

--- module2/2.16/script.js
+++ module2/2.16/script.js
@@ -30,3 +30,52 @@ btnGen.addEventListener("click", () => {
   ul.insertAdjacentElement("beforeend", li);
   number++;
 });
+
+//prepod solutions
+/**
+ * /1
+ const btn = document.getElementById('btn-msg');
+    btn.addEventListener('click', (e) => {
+    //   alert(e.target.dataset.text);
+    // или если элемент один можно обратится к нему
+    //   alert(btn.dataset.text);
+    // так же внутри обработчика события через this мы имеем доступ к элементу на который повешено событие
+    //   alert(this.dataset.text);
+    // или через currentTarget
+        alert(e.currentTarget.dataset.text);
+    });
+
+      /2
+     const btn = document.getElementById('btn-msg');
+    // У событий mouseover и mouseout есть аналоги mouseenter/mouseleave
+    btn.addEventListener('mouseover', (e) => {
+      btn.classList.add('bg-red');
+    });
+    btn.addEventListener('mouseout', (e) => {
+      btn.classList.remove('bg-red');
+    });
+
+    /3
+    const tagTextContainer = document.getElementById('tag');
+    document.body.addEventListener('click', (e) => {
+      tagTextContainer.textContent = `Tag: ${e.target.nodeName}`;
+    });
+
+    /4
+    
+    const generateBtn = document.getElementById('btn-generate');
+    const list = document.querySelector('ul');
+
+    function generateNewItem(e) {
+      // получаем текущее количество элементов в списке
+      const num = list.children.length + 1;
+      const text = `Item ${num}`;
+      const li = document.createElement('li');
+      li.textContent = text;
+
+      list.appendChild(li);
+    }
+
+    generateBtn.addEventListener('click', generateNewItem);
+
+ */
