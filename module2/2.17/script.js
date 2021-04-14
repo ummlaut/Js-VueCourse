9702560b196fde2e09f370f8c2244d5312dcfd03
ummlaut <iachmiz@gmail.com>
2021-04-15 02:22:51 +0300

    module 2 hw 17 with solution

--- module2/2.17/script.js
+++ module2/2.17/script.js
@@ -11,3 +11,27 @@ for (item of items) {
     }
   });
 }
+/**
+ * /prepod solution
+ */
+const dropdownItems = document.querySelectorAll(".dropdown-item");
+// В данной переменной мы будем хранить текущее открытое меню
+let currentOpenedMenu = null;
+
+function toggleDropdownMenu(e) {
+  // получаем блок меню внутри .dropdown-item
+  const menu = e.currentTarget.querySelector(".dropdown-menu");
+  // метод toggle возвращает булевое значение, если клас удален то вернет false а если добавлен то true. Это значение мы сохраняем в переменную
+  const isAdded = menu.classList.toggle("d-none");
+  // если в currentOpenedMenu уже есть блок и он не равен текущем menu то мы его скрываем
+  if (currentOpenedMenu && currentOpenedMenu !== menu) {
+    currentOpenedMenu.classList.add("d-none");
+  }
+  // Если класс d-none был удален то значит меню видимо и мы его сохраняем в переменную currentOpenedMenu
+  if (!isAdded) {
+    currentOpenedMenu = menu;
+  }
+}
+
+dropdownItems.forEach((d) => d.addEventListener("click", toggleDropdownMenu));
+*/
